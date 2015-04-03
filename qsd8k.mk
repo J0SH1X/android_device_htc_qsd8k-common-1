b5fd09bcb77596b40ce4e29a8a037703fe18055a
spezi77 <spezi7713@gmail.com>
2015-04-03 10:51:22 +0200

    Add KK specific HWUI tweaks by pixelfreak & walter79
    Change-Id: I61047f12722af212df50412b25680ffd7f299169

--- qsd8k.mk
+++ qsd8k.mk
@@ -77,14 +77,98 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwc.fakevsync=1 \
     debug.composition.type=mdp \
     ro.zygote.disable_gl_preload=true \
-    debug.gr.numframebuffers=2
+    debug.gr.numframebuffers=2 \
+    debug.performance.tuning=1 \
+    persist.sys.ui.hw=1 \
+    video.accelerate.hw=1 \
+    view.scroll_friction=0
 
 #
-# Low Mem
+# HWUI TWEAKS (credits pixelfreak & walter79)
+#
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.disable_scissor_opt=false \
+    ro.hwui.texture_cache_size=24 \
+    ro.hwui.layer_cache_size=16 \
+    ro.hwui.gradient_cache_size=0.5 \
+    ro.hwui.patch_cache_size=128 \
+    ro.hwui.path_cache_size=4 \
+    ro.hwui.shape_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=2 \
+    ro.hwui.r_buffer_cache_size=2 \
+    ro.hwui.texture_cache_flush_rate=0.6 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=256 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=512 \
+    hwui.text_gamma_correction=lookup \
+    hwui.text_gamma=1.4 \
+    hwui.text_gamma.black_threshold=64 \
+    hwui.text_gamma.white_threshold=192 \
+    hwui.use_gpu_pixel_buffers=true \
+    hwui.render_dirty_regions=false \
+    hwui.use.blacklist=true
+
+# DISABLE ERROR CHECKING
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.kernel.android.checkjni=0
+
+# MULTI-CORE DEX OPT DISABLED
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.dalvik.multithread=false
+
+#
+# Low Mem tweaks
 #
 PRODUCT_PROPERTY_OVERRIDES += ro.config.low_ram=true
+
 # Disable jit
 PRODUCT_PROPERTY_OVERRIDES += dalvik.vm.jit.codecachesize=0
+
+# Allows purging of assets to free up RAM
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.purgeable_assets=1
+
+# Disable atlas services on low-ram targets
+PRODUCT_PROPERTY_OVERRIDES += config.disable_atlas=true
+
+#
+# Battery tweaks
+#
+# Caution: ro.mot.eri.losalert.delay=1000 (might break tethering)
+# pm.sleep_mode=0 := MSM_PM_SLEEP_MODE_POWER_COLLAPSE_SUSPEND
+# pm.sleep_mode=1 := MSM_PM_SLEEP_MODE_POWER_COLLAPSE
+#
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mot.eri.losalert.delay=1000 \
+    pm.sleep_mode=1 \
+    ro.ril.power_collapse=1
+    
+#
+# Telephony/ring Tweaks
+#
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.call_ring.delay=3000
+
+#
+# Proximity (Disable blackscreen issue after call)
+#
+PRODUCT_PROPERTY_OVERRIDES += \
+    mot.proximity.delay=25 \
+    ro.lge.proximity.delay=25 \
+    ring.delay=0
+
+#
+# Scrolling tweaks
+#
+# windowsmgr.max_events_per_sec := 60..300
+#
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.min_pointer_dur=8 \
+    ro.max.fling_velocity=12000 \
+    ro.min.fling_velocity=8000 \
+    windowsmgr.max_events_per_sec=120
 
 #
 # Dalvik Properties
@@ -92,6 +176,10 @@ PRODUCT_PROPERTY_OVERRIDES += dalvik.vm.jit.codecachesize=0
 
 # Default heap settings for 512mb device
 include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
+
+# MAKES APPS LOAD FASTER AND FREES MORE RAM
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dexopt-flags=v=a,o=v,m=y,u=y
 
 # We have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
@@ -115,7 +203,7 @@ $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=45
+    wifi.supplicant_scan_interval=120
 
 #
 # Qcom
