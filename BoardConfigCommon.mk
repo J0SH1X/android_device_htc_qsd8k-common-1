af57a09c259f7f5eac95545e0b57ca399cfaa6fc
spezi77 <spezi7713@gmail.com>
2015-05-03 17:47:56 +0200

    Change kernel target toolchain to 4.7
    Change-Id: I33b3a34d586f224fb2573aea2063ce5a42c7af31

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,12 +99,12 @@ COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 BOARD_USE_OLD_AVC_ENCODER := true
 BOARD_NO_BFRAMES := true
 
-# Kernel directory
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/htc/qsd8k
 BUILD_KERNEL := true
 
-# Override kernel toolchain. (4.6 is too unstable)
-KERNEL_TOOLCHAIN_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.4.3/bin/arm-eabi-
+# Override kernel toolchain
+KERNEL_TOOLCHAIN_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin/arm-eabi-
 
 # TWRP
 DEVICE_RESOLUTION := 480x800
@@ -114,3 +114,4 @@ TW_NO_SCREEN_BLANK := true
 TW_EXCLUDE_ENCRYPTED_BACKUPS := true
 TW_USE_TOOLBOX := true
 TW_EXCLUDE_SUPERSU := true
+
