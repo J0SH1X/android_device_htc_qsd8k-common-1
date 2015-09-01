LineageOS/android_device_oppo_msm8974-common

build: Avoid poisoning the host binaries with bad ABIs
the vector class symbols are only needed on the target. Don't inject
(or expect) them into the host binaries to avoid multi-device build
conflicts and ABI breakage

Change-Id: I224678f172bf38b820f507b1ec8540d60b865772

BoardConfigCommon.mk
@@ -47,8 +47,8 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
 
 # Flags
 COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+TARGET_GLOBAL_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true

J0SH1X/android_device_htc_qsd8k-common-1

Signed-off-by: spezi77 <spezi77@gmx.com>

BoardConfigCommon.mk
@@ -48,7 +48,7 @@ BOARD_BLUEDROID_VENDOR_CONF := device/htc/qsd8k-common/bluetooth/vnd_qsd8k.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/qsd8k-common/bluetooth/include
 
 # Camera
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+TARGET_RELEASE_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 BOARD_USE_OLD_AVC_ENCODER := true
 BOARD_NO_BFRAMES := true
 BOARD_USES_QCOM_LEGACY_CAM_PARAMS := true